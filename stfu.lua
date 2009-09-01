aa98c1759586f7e8e72c17b30c32b771eb2a3bf4
Trond A Ekseth <troeks@gmail.com>
2009-09-01 21:32:33 +0200

    Block raid warnings.

--- stfu.lua
+++ stfu.lua
@@ -2,3 +2,13 @@ ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", function(self, event, msg)
 	return msg:sub(1, 5) == '<DBM>'
 end)
 
+ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID_WARNING", function(self, event, msg)
+	return msg:sub(1,3) == "***" and msg:sub(-3) == "***"
+end)
+
+local org = RaidNotice_AddMessage
+function RaidNotice_AddMessage(noticeFrame, msg, colorInfo, ...)
+	if(not (msg:sub(1,3) == "***" and msg:sub(-3) == "***")) then
+		return org(noticeFrame, msg, colorInfo, ...)
+	end
+end
